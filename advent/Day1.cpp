cd190a4d4deafbfbbfd79c3349f56bdd3039ce8f
Joao Afonso <joao.afonso@cern.ch>
2025-12-10 01:23:19 +0100

    Added tests

--- advent/Day1.cpp
+++ advent/Day1.cpp
@@ -1,13 +1,37 @@
 #include "../common/AutoRegister.hpp"
+#include "Day1.hpp"
 
 namespace advent {
 
-class Day1 : public common::BaseDay {
-public:
-    common::AocResultType run(std::vector<std::string> input) override {
-        return std::pair(1, 1);
+common::AocResultType Day1::run(std::vector<std::string> input) {
+
+    // Part 1 & 2
+
+    int result1 = 0;
+    int result2 = 0;
+    int currentposition = 50;
+    for (auto & line : input) {
+        const char lr = line[0];
+        const int value = std::stoi(line.substr(1));
+        if (lr == 'L') {
+            auto temp_position = currentposition - value;
+            result2 += (-temp_position / 100);
+            if (temp_position < 0 && currentposition > 0) {
+                result2++;
+            }
+            currentposition = ((temp_position % 100) + 100) % 100;
+        } else {
+            auto temp_position = currentposition + value;
+            result2 += (temp_position / 100);
+            currentposition = temp_position % 100;
+        }
+        if (currentposition == 0) {
+            result1++;
+        }
     }
-};
+
+    return std::pair(result1, result2);
+}
 
 // To force registration at startup
 namespace {
--- tests/DayTests.cpp
+++ tests/DayTests.cpp
@@ -1,7 +1,25 @@
 #include <gtest/gtest.h>
+#include "../advent/Day1.hpp"
 
 namespace advent::tests {
+
 TEST(advent, day_1) {
-    ASSERT_TRUE(false);
+    std::vector<std::string> test_data = {
+        "L68",
+        "L30",
+        "R48",
+        "L5",
+        "R60",
+        "L55",
+        "L1",
+        "L99",
+        "R14",
+        "L82",
+    };
+    auto day1 = Day1();
+    auto result = day1.run(test_data);
+    ASSERT_EQ(result.first, 3);
+    ASSERT_EQ(result.second, 6);
 }
+
 }
