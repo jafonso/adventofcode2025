55d216b3c9b6edeac6e33e404a9d18d0e21a70da
Joao Afonso <joao.afonso@cern.ch>
2025-12-19 00:48:36 +0000

    Day 2 part 2

--- advent/Day2.cpp
+++ advent/Day2.cpp
@@ -1,5 +1,6 @@
 #include "../common/AutoRegister.hpp"
 
+#include <set>
 #include <list>
 #include <ranges>
 #include <string>
@@ -31,6 +32,8 @@ public:
         std::list<std::pair<std::string, std::string>> input_ranges_str_tmp;
         std::list<std::pair<uint64_t, uint64_t>> input_ranges;
 
+        // Parse the input into `input_ranges_str` and `input_ranges`
+
         while (!input_view.empty()) {
             auto item_end_pos = input_view.find(',');
             auto pair_str = input_view.substr(0, item_end_pos);
@@ -54,86 +57,77 @@ public:
             }
         }
 
-        // Part 1
+        // Split into same length segments
 
-        uint64_t result_1 = 0;
-        std::list<std::pair<uint64_t, uint64_t>> repeated_ranges;
+        std::list<std::pair<std::string, std::string>> input_ranges_str;
 
-        // Split into valid sub-ranges
         for (auto & [str_begin, str_end] : input_ranges_str_tmp) {
-
-            std::cout << str_begin << "-" << str_end << std::endl;
-
-            uint64_t val_begin_1, val_begin_2;
-            if (auto len_begin = str_begin.size(); len_begin % 2 == 0) {
-                std::from_chars(str_begin.c_str(), str_begin.c_str() + (len_begin/2), val_begin_1);
-                std::from_chars(str_begin.c_str() + (len_begin/2), str_begin.c_str() + len_begin, val_begin_2);
-            }
-
-            uint64_t val_end_1, val_end_2;
-            if (auto len_end = str_end.size(); len_end % 2 == 0) {
-                std::from_chars(str_end.c_str(), str_end.c_str() + (len_end/2), val_end_1);
-                std::from_chars(str_end.c_str() + (len_end/2), str_end.c_str() + len_end, val_end_2);
-            }
-
             for (auto len = str_begin.size(); len <= str_end.size(); len++) {
-                // Repeated numbers must have even number of digits
-                if ((len % 2) == 1) {
-                    continue;
-                }
+
                 bool is_smallest_len = (len == str_begin.size());
                 bool is_largest_len = (len == str_end.size());
 
                 if (is_smallest_len && is_largest_len) {
-                    repeated_ranges.emplace_back(
-                        val_begin_1 >= val_begin_2 ? val_begin_1 : val_begin_1 + 1,
-                        val_end_1 <= val_end_2 ? val_end_1 + 1 : val_end_1
+                    input_ranges_str.emplace_back(
+                        str_begin,
+                        std::to_string(std::stoull(str_end) + 1)
                         );
                 } else if (is_smallest_len) {
-                    repeated_ranges.emplace_back(
-                        val_begin_1 >= val_begin_2 ? val_begin_1 : val_begin_1 + 1,
-                        pow10(len/2)
+                    input_ranges_str.emplace_back(
+                        str_begin,
+                        std::to_string(pow10(len))
                         );
                 } else if (is_largest_len) {
-                    repeated_ranges.emplace_back(
-                        pow10(len/2 - 1),
-                        val_end_1 <= val_end_2 ? val_end_1 + 1 : val_end_1
+                    input_ranges_str.emplace_back(
+                        std::to_string(pow10(len-1)),
+                        std::to_string(std::stoull(str_end) + 1)
                         );
                 } else {
-                    repeated_ranges.emplace_back(
-                        pow10(len/2 - 1),
-                        pow10(len/2)
+                    input_ranges_str.emplace_back(
+                        std::to_string(pow10(len-1)),
+                        std::to_string(pow10(len))
                         );
                 }
-
-                std::cout << "[" << repeated_ranges.back().first << ", " << repeated_ranges.back().second << ")" << std::endl;
-
-                uint64_t res_tmp = sumRange(repeated_ranges.back().first, repeated_ranges.back().second);
-                result_1 += res_tmp;
-                std::cout << result_1 << " (+" << res_tmp << ")" << std::endl;
             }
-
         }
 
         // Part 1
 
+        uint64_t result_1 = 0;
+        std::set<uint64_t> used_values_1;
 
+        for (auto & [str_begin, str_end] : input_ranges_str) {
+            result_1 += sumRange(str_begin, str_end, 2, used_values_1);
+        }
 
-        //std::num_di
+        // Part 2
 
-        //for (auto & [a, b] : input_ranges_str_tmp) {
-        //    std::cout << a << "-" << b << std::endl;
-        //}
+        uint64_t result_2 = 0;
+        std::set<uint64_t> used_values_2;
 
+        for (auto & [str_begin, str_end] : input_ranges_str) {
+            for (auto num_parts = str_begin.size(); num_parts >= 2; num_parts--) {
+                result_2 += sumRange(str_begin, str_end, num_parts, used_values_2);
+            }
+        }
 
-
-        return std::pair(result_1, 0);
+        return std::pair(result_1, result_2);
     }
 private:
     static constexpr uint64_t pow10(uint64_t n) {
-        uint64_t r = 1;
-        while (n--) r *= 10;
-        return r;
+        std::array<uint64_t, 10> POW_10 = {
+            1L,
+            10L,
+            100L,
+            1000L,
+            10000L,
+            100000L,
+            1000000L,
+            10000000L,
+            100000000L,
+            1000000000L
+        };
+        return POW_10[n];
     }
     static constexpr uint64_t valLen(uint64_t n) {
         uint64_t r = 0;
@@ -148,6 +142,60 @@ private:
         auto tmp = (end - begin) * (begin + end - 1) / 2;
         tmp = tmp * pow10(len) + tmp;
         return tmp;
+    }
+
+    static uint64_t sumRange(const std::string & str_begin, const std::string & str_end, uint64_t num_parts, std::set<uint64_t> & used_values) {
+
+        auto len = str_begin.size();
+        // Ignore if we cannot split in 'num_parts'
+        if (len % num_parts != 0) {
+            return 0;
+        }
+
+        uint64_t num_begin_prefix, num_end_prefix;
+        const auto len_prefix = len / num_parts;
+
+        {
+            std::from_chars(str_begin.c_str(), str_begin.c_str() + len_prefix, num_begin_prefix);
+            uint64_t tmp_begin = calculateNumber(num_begin_prefix, len_prefix, num_parts);
+            if (tmp_begin < std::stoull(str_begin)) {
+                num_begin_prefix++;
+            }
+        }
+
+        {
+            if (str_begin.size() == str_end.size()) {
+                std::from_chars(str_end.c_str(), str_end.c_str() + len_prefix, num_end_prefix);
+                uint64_t tmp_end = calculateNumber(num_end_prefix, len_prefix, num_parts);
+                if (tmp_end < std::stoull(str_end)) {
+                    num_end_prefix++;
+                }
+            } else {
+                std::from_chars(str_end.c_str(), str_end.c_str() + len_prefix + 1, num_end_prefix);
+            }
+        }
+
+        uint64_t sum = 0;
+
+        for (uint64_t val_prefix = num_begin_prefix; val_prefix < num_end_prefix; val_prefix++) {
+            uint64_t value = calculateNumber(val_prefix, len_prefix, num_parts);
+            if (used_values.contains(value)) {
+                // Ignore if value has already been counted
+                continue;
+            }
+            used_values.insert(value);
+            sum += value;
+        }
+
+        return sum;
+    }
+
+    static uint64_t calculateNumber(uint64_t prefix, std::size_t len_prefix, std::size_t num_parts) {
+        uint64_t value = prefix;
+        for (std::size_t i = 1; i < num_parts; i++) {
+            value = value * pow10(len_prefix) + prefix;
+        }
+        return value;
     }
 };
 
