0a8c220d3933ea83b244138589c28d1b05309073
Joao Afonso <joao.afonso@cern.ch>
2025-12-21 20:11:52 +0000

    Finished day 9

--- advent/Day9.cpp
+++ advent/Day9.cpp
@@ -39,7 +39,7 @@ public:
 
         for (auto it_1 = std::ranges::begin(all_tiles); it_1 != std::ranges::end(all_tiles); ++it_1) {
             for (auto it_2 = std::next(it_1); it_2 != std::ranges::end(all_tiles); ++it_2) {
-                uint64_t area = (it_1->x <= it_2->x) ? rectArea(*it_1, *it_2) : rectArea(*it_2, *it_1);
+                uint64_t area = rectArea(*it_1, *it_2);
                 squares_by_size.emplace(area, (it_1->x <= it_2->x) ? std::make_pair(*it_1, *it_2) : std::make_pair(*it_2, *it_1)); // This is for Part 2
                 if (area > best_rect_area_1) {
                     best_rect_area_1 = area;
@@ -54,56 +54,47 @@ public:
         uint64_t best_rect_area_2 = 0;
         auto [x_lines_set, y_lines_set] = getXYParallelLines(all_tiles);
 
-        //drawBoard(all_tiles);
-
         bool found = false;
         while (!found && !squares_by_size.empty()) {
-
-            std::cout << std::endl << "--------------------" << std::endl;
 
             // Calculate limits of the rectangle
             auto [size, tile_pair] = squares_by_size.top();
             squares_by_size.pop();
             auto & [tile_1, tile_2] = tile_pair;
-            squares_by_size.pop();
 
             auto x_min = tile_1.x < tile_2.x ? tile_1.x : tile_2.x;
             auto x_max = tile_1.x > tile_2.x ? tile_1.x : tile_2.x;
             auto y_min = tile_1.y < tile_2.y ? tile_1.y : tile_2.y;
             auto y_max = tile_1.y > tile_2.y ? tile_1.y : tile_2.y;
 
-            std::cout << "x[" << x_min << ", " << x_max << "] y[" << y_min << ", " << y_max << "]" << std::endl;
-
             // Get iterator for X and Y lines in between the square limits
-            if (x_max > x_min + 1) {
+            {
                 auto x_it_begin = x_lines_set.upper_bound(x_min);
                 auto x_it_end = x_lines_set.lower_bound(x_max);
                 if (x_it_begin != x_it_end) {
                     // Check all X lines within the square limits
                     for (auto it = x_it_begin; it != x_it_end; ++it) {
                         auto & [tile_min, tile_max] = it->second;
-                        std::cout << "X line min/max: (" << tile_min.x << ", " << tile_min.y << ") (" << tile_max.x << ", " << tile_max.y << ")" << std::endl;
                         // When line is fully inside
                         if (tile_min.y >= y_min && tile_max.y <= y_max) goto bypass;
                         // When line is partially inside
-                        if (tile_min.y > y_min && tile_min.y < y_max) goto bypass;
-                        if (tile_max.y > y_min && tile_max.y < y_max) goto bypass;
+                        if (tile_min.y <= y_min && tile_max.y > y_min) goto bypass;
+                        if (tile_min.y < y_max && tile_max.y >= y_max) goto bypass;
                     }
                 }
             }
-            if (y_max > y_min + 1) {
+            {
                 auto y_it_begin = y_lines_set.upper_bound(y_min);
                 auto y_it_end = y_lines_set.lower_bound(y_max);
                 if (y_it_begin != y_it_end) {
                     // Check all Y lines within the square limits
                     for (auto it = y_it_begin; it != y_it_end; ++it) {
                         auto & [tile_min, tile_max] = it->second;
-                        std::cout << "Y line min/max: (" << tile_min.x << ", " << tile_min.y << ") (" << tile_max.x << ", " << tile_max.y << ")" << std::endl;
                         // When line is fully inside
                         if (tile_min.x >= x_min && tile_max.x <= x_max) goto bypass;
                         // When line is partially inside
-                        if (tile_min.x > x_min && tile_min.x < x_max) goto bypass;
-                        if (tile_max.x > x_min && tile_max.x < x_max) goto bypass;
+                        if (tile_min.x <= x_min && tile_max.x > x_min) goto bypass;
+                        if (tile_min.x < x_max && tile_max.x >= x_max) goto bypass;
                     }
                 }
             }
@@ -112,17 +103,17 @@ public:
             {
                 uint64_t lines_crossed = 0;
                 uint64_t y_ref = (y_max + y_min) / 2;
-                auto x_ref_begin = x_lines_set.lower_bound(x_max);
+                uint64_t x_ref = (x_max + x_min) / 2;
+                auto x_ref_begin = x_lines_set.lower_bound(x_ref);
                 for (auto it = x_ref_begin; it != x_lines_set.end(); ++it) {
                     auto & [tile_min, tile_max] = it->second;
-                    if (tile_min.y <= y_ref && tile_max.y >= y_ref) {
+                    if (tile_min.y <= y_ref && tile_max.y > y_ref) {
                         lines_crossed++;
                     }
                 }
-                if (lines_crossed % 2) goto bypass;
+                if ((lines_crossed % 2) == 0) goto bypass;
             }
             // Finally we are at the end. The rectangle is inside.
-            std::cout << "Size: " << size << " (" << tile_1.x << ", " << tile_1.y << ") (" << tile_2.x << ", " << tile_2.y << ")" << std::endl;
             best_rect_area_2 = size;
             found = true;
             // Go back to loop
@@ -134,21 +125,6 @@ public:
         return std::pair(result_1, result_2);
     }
 private:
-    static void drawBoard(const std::vector<Tile> & tiles) {
-        std::size_t max_x = 0, max_y = 0;
-        for (const auto & tile : tiles) {
-            if (tile.x + 2 > max_x) max_x = tile.x + 2;
-            if (tile.y + 2 > max_y) max_y = tile.y + 2;
-        }
-
-        std::vector board(max_y, std::string(max_x, '.'));
-        for (const auto & [x, y] : tiles) {
-            board[y][x] = '#';
-        }
-        for (auto & line : board) {
-            std::cout << line << std::endl;
-        }
-    }
     static std::pair<std::multimap<uint64_t, Line>, std::multimap<uint64_t, Line>> getXYParallelLines(const std::vector<Tile> & tiles) {
         Tile old_tile = tiles.back();
         std::multimap<uint64_t, Line> output_x;
